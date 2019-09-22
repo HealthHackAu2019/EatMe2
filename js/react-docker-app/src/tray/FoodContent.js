19fd8f5e4feb365aca3925f1be09ac5af9031128
Prithvi Reddy <prithvi@thaum.io>
2019-09-22 13:24:21 +1000

    flex center

--- js/react-docker-app/src/tray/FoodContent.js
+++ js/react-docker-app/src/tray/FoodContent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Progress, Col, Row } from 'reactstrap'
+import { Progress, Col, Toast, ToastHeader, ToastBody, Row} from 'reactstrap'
 import map from '../mock_data/icon2label'
 
 
@@ -8,23 +8,25 @@ class FoodBar extends Component {
         const { icon, title, percent } = this.props
         console.log('data/img/icons/'+map[title])
         return (
-            <Col>
-                <Row centered>
-                    <h7> {title} </h7>
-                </Row>
-                <Row>
+            <Toast>
+                <ToastHeader>
+                    {title}
+                </ToastHeader>
+                <ToastBody>
+                    <Row>
                     <Col sm={2}>
                         <img src={'data/img/icons/'+map[title]}/>
                     </Col>
-                    <Col sm={10}>
-                    <Progress 
-                        value={percent}
-                        color="green"
-                        // type="circle"
-                    />
+                    <Col sm={10} style={{ display: 'flex', flexDirection: 'column',  justifyContent: 'center'}}>
+                        <Progress 
+                            value={percent}
+                            color="green"
+                            // type="circle"
+                        />
                     </Col>
-                </Row>
-            </Col>
+                    </Row>
+                </ToastBody>
+            </Toast>
         )
     }
 }
