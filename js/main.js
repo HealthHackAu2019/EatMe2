52313c8d4353a90a4f0a5f75dc1b3b6b9a80899d
karol <karol.pajak@nuix.com>
2019-09-22 14:58:37 +1000

    graph

--- js/main.js
+++ js/main.js
@@ -1,3 +1,162 @@
+function makeLinks(data) {
+    let cats = {};
+    let links = [];
+
+    data.forEach(d => {
+        if (!cats[d.category_id]) {
+            cats[d.category_id] = {
+                id: d.category_id,
+                max: d.area,
+                eaten: 0,
+                waste: 0
+            }
+        } else {
+            cats[d.category_id].max = Math.max(d.area, cats[d.category_id].max)
+        }
+    });
+
+    data.forEach(d => {
+        let eaten = cats[d.category_id].max - d.area;
+        cats[d.category_id].eaten += eaten;
+        cats[d.category_id].waste += d.area;
+    });
+
+    console.log(cats);
+
+    for (let catIdx in cats) {
+        let cat = cats[catIdx];
+        links.push({
+            source: "C" + catIdx,
+            target: "P",
+            value: cat.eaten
+        });
+        links.push({
+            source: "C" + catIdx,
+            target: "B",
+            value: cat.waste
+        });
+    }
+
+    return links;
+}
+
+function plotGraph() {
+
+    let ignored_cats = [1, 8, 9, 10, 12, 15];
+
+    let nodes = exports.masks.categories
+        .filter(d => !ignored_cats.includes(d.id))
+        .map(d => {
+            return {
+                id: "C" + d.id,
+                name: d.name,
+            }
+        });
+
+    nodes = nodes.concat([{
+        id: "P",
+        name: "Patient",
+    }, {
+        id: "B",
+        name: "Bin",
+    }]);
+
+    let links = makeLinks(exports.masks.annotations
+        .filter(d => !ignored_cats.includes(d.category_id)));
+
+    const width = 800;
+    const height = 400;
+    let edgeColor = 'output';
+    let align = ['justified'];
+
+    const svg = d3.select("#graph")
+        .append("svg")
+        .attr("viewBox", [0, 0, width, height]);
+
+    const sankey = d3.sankey()
+    // .nodeAlign(d3[`sankey${align[0].toUpperCase()}${align.slice(1)}`])
+        .nodeWidth(15)
+        .nodePadding(10)
+        .nodeId(n => n.id)
+        .extent([[1, 5], [width - 1, height - 5]]);
+
+    let graph = sankey({
+        nodes: nodes,
+        links: links
+    });
+
+    function format(d) {
+        const f = d3.format(",.0f");
+        return `${f(d)} TWh`;
+    }
+
+    function color(name) {
+        if (name === 'Bin') return 'red';
+        if (name === 'Patient') return 'green';
+        return d3.scaleOrdinal(d3.schemeCategory10)(name);
+    }
+
+    svg.append("g")
+        .attr("stroke", "#000")
+        .selectAll("rect")
+        .data(nodes)
+        .join("rect")
+        .attr("x", d => d.x0)
+        .attr("y", d => d.y0)
+        .attr("height", d => d.y1 - d.y0)
+        .attr("width", d => d.x1 - d.x0)
+        .attr("fill", d => color(d.name))
+        .append("title")
+        .text(d => `${d.name}\n${format(d.value)}`);
+
+    const link = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke-opacity", 0.5)
+        .selectAll("g")
+        .data(links)
+        .join("g")
+        .style("mix-blend-mode", "multiply");
+
+    if (edgeColor === "path") {
+        const gradient = link.append("linearGradient")
+            .attr("id", d => (d.uid = DOM.uid("link")).id)
+            .attr("gradientUnits", "userSpaceOnUse")
+            .attr("x1", d => d.source.x1)
+            .attr("x2", d => d.target.x0);
+
+        gradient.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", d => color(d.source.name));
+
+        gradient.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", d => color(d.target.name));
+    }
+
+    link.append("path")
+        .attr("d", d3.sankeyLinkHorizontal())
+        .attr("stroke", d => edgeColor === "none" ? "#aaa"
+            : edgeColor === "path" ? d.uid
+                : edgeColor === "input" ? color(d.source.name)
+                    : color(d.target.name))
+        .attr("stroke-width", d => Math.max(1, d.width));
+
+    link.append("title")
+        .text(d => `${d.source.name} → ${d.target.name}\n${format(d.value)}`);
+
+    svg.append("g")
+        .style("font", "10px sans-serif")
+        .selectAll("text")
+        .data(nodes)
+        .join("text")
+        .attr("x", d => d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6)
+        .attr("y", d => (d.y1 + d.y0) / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
+        .text(d => d.name);
+}
+
+
 /**
  * Fetches the frame image and associated data from files in `/data`,
  * then plots the image in given `targetElement`,
